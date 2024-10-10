25eeac78f72176f547eef55ad2e5dfad5386eec8
pilar manavella <pilarmanavella@gmail.com>
2024-10-10 09:23:49 -0300

    dto cursos

---
+++ courses-api/dto_curso.go
@@ -0,0 +1,66 @@
+package coursesapi
+
+//dto es domain
+
+type CoursesResponse_Full []CourseResponse_Full // si está en plural son muchos
+
+type CoursesResponse_Result []CourseResponse_Result
+
+type CoursesRequest_Category []CourseRequest_Category
+
+type CoursesRequest_Title []CourseRequest_Title
+
+type CoursesRequest_Description []CourseRequest_Description
+
+// BUSQUEDA DE CURSOS
+
+type CourseRequest_Category struct {
+	Category string `json:"category"`
+}
+
+type CourseRequest_Title struct {
+	Title string `json:"title"`
+}
+
+type CourseRequest_Description struct {
+	Description string `json:"description"`
+}
+
+type CourseRequest_Token struct {
+	Token string `json:"token"`
+}
+
+// RESULTADO LISTANDO CURSOS
+
+type CourseResponse_Result struct {
+	ID_Course   int     `json:"id_course"`
+	Title       string  `json:"title"`
+	Description string  `json:"description"`
+	ImageURL    string  `json:"image_url"`
+	Rating      float64 `json:"rating"`
+}
+
+// DETALLE DEL CURSO
+
+type CourseResponse_Full struct {
+	ID_Course    int    `json:"id_course"`
+	Title        string `json:"title"`
+	Description  string `json:"description"`
+	Category     string `json:"category"`
+	ImageURL     string `json:"image_url"`
+	Duration     string `json:"duration"`
+	Instructor   string `json:"instructor"`
+	Requirements string `json:"requirements"`
+}
+
+// esto va a ir en Registration
+
+// // INSCRIPCION EN CURSO
+// type CourseRequest_Registration struct {
+// 	Token     string `json:"token"`
+// 	ID_Course int    `json:"course_id"`
+// }
+
+// type CourseResponse_Registration struct {
+// 	ID_Course int `json:"course_id"`
+// }
