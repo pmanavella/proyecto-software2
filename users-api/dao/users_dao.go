7ccb3f371c453967e11980bb11507c243b3113fe
Cervellini2501 <2200169@ucc.edu.ar>
2024-10-30 10:56:58 -0300

    actualización users-api

--- users-api/dao/users_dao.go
+++ users-api/dao/users_dao.go
@@ -1,7 +1,85 @@
 package users
 
+import (
+    "gorm.io/gorm"
+    "users-api/domain/users" // Importar el paquete de domain
+)
+
 type User struct {
-	ID       int64
-	Username string
-	Password string
+	ID       int64  `gorm:"primaryKey;autoIncrement"`                    // Auto-increment primary key
+	Username string `gorm:"size:100;not null;unique" binding:"required"` // Unique username, required
+	Password string `gorm:"size:255;not null" binding:"required"`        // Password field, required
 }
+
+func CreateUser(db *gorm.DB, user users.User) (int64, error) {
+    // Convertir `users.User` a `UserDAO`
+    userDAO := User{
+        Username: user.Username,
+        Password: user.Password,
+    }
+
+    if err := db.Create(&userDAO).Error; err != nil {
+        return 0, err
+    }
+
+    return userDAO.ID, nil
+}
+
+func GetUserByID(db *gorm.DB, id int64) (users.User, error) {
+    var userDAO User
+    if err := db.First(&userDAO, id).Error; err != nil {
+        return users.User{}, err
+    }
+
+    // Convertir `UserDAO` a `users.User`
+    user := users.User{
+        ID:       userDAO.ID,
+        Username: userDAO.Username,
+        Password: userDAO.Password,
+    }
+
+    return user, nil
+}
+
+func GetUserByUsername(db *gorm.DB, username string) (users.User, error) {
+    var userDAO User
+    if err := db.Where("username = ?", username).First(&userDAO).Error; err != nil {
+        return users.User{}, err
+    }
+
+    // Convertir `UserDAO` a `users.User`
+    user := users.User{
+        ID:       userDAO.ID,
+        Username: userDAO.Username,
+        Password: userDAO.Password,
+    }
+
+    return user, nil
+}
+
+func UpdateUser(db *gorm.DB, user users.User) error {
+    userDAO := User{
+        ID:       user.ID,
+        Username: user.Username,
+        Password: user.Password,
+    }
+
+    if err := db.Save(&userDAO).Error; err != nil {
+        return err
+    }
+    return nil
+}
+
+
+func DeleteUser(db *gorm.DB, id int64) error {
+    if err := db.Delete(&User{}, id).Error; err != nil {
+        return err
+    }
+    return nil
+}
+
+
+
+
+
+
--- users-api/repositories/users_cache.go
+++ users-api/repositories/users_cache.go
@@ -1,1 +1,93 @@
 package users
+
+import (
+    "fmt"
+    "github.com/karlseguin/ccache"
+    "time"
+    "users-api/domain/users"
+)
+
+type CacheConfig struct {
+    TTL time.Duration // Cache expiration time
+}
+
+type Cache struct {
+    client *ccache.Cache
+    ttl    time.Duration
+}
+
+func NewCache(config CacheConfig) Cache {
+    // Initialize ccache with default settings
+    cache := ccache.New(ccache.Configure())
+    return Cache{
+        client: cache,
+        ttl:    config.TTL,
+    }
+}
+
+// Helper functions to generate cache keys
+func cacheKeyByID(id int64) string {
+    return fmt.Sprintf("user:id:%d", id)
+}
+
+func cacheKeyByUsername(username string) string {
+    return fmt.Sprintf("user:username:%s", username)
+}
+
+func (repository Cache) GetAll() ([]users.User, error) {
+    return nil, fmt.Errorf("GetAll not implemented in cache")
+}
+
+func (repository Cache) GetByID(id int64) (users.User, error) {
+    idKey := cacheKeyByID(id)
+    item := repository.client.Get(idKey)
+    if item != nil && !item.Expired() {
+        user, ok := item.Value().(users.User)
+        if !ok {
+            return users.User{}, fmt.Errorf("cached value is not of type User")
+        }
+        return user, nil
+    }
+    return users.User{}, fmt.Errorf("cache miss for user ID %d", id)
+}
+
+func (repository Cache) GetByUsername(username string) (users.User, error) {
+    userKey := cacheKeyByUsername(username)
+    item := repository.client.Get(userKey)
+    if item != nil && !item.Expired() {
+        user, ok := item.Value().(users.User)
+        if !ok {
+            return users.User{}, fmt.Errorf("cached value is not of type User")
+        }
+        return user, nil
+    }
+    return users.User{}, fmt.Errorf("cache miss for username %s", username)
+}
+
+func (repository Cache) Create(user users.User) (int64, error) {
+    idKey := cacheKeyByID(user.ID)
+    userKey := cacheKeyByUsername(user.Username)
+    repository.client.Set(idKey, user, repository.ttl)
+    repository.client.Set(userKey, user, repository.ttl)
+    return user.ID, nil
+}
+
+func (repository Cache) Update(user users.User) error {
+    idKey := cacheKeyByID(user.ID)
+    userKey := cacheKeyByUsername(user.Username)
+    repository.client.Set(idKey, user, repository.ttl)
+    repository.client.Set(userKey, user, repository.ttl)
+    return nil
+}
+
+func (repository Cache) Delete(id int64) error {
+    idKey := cacheKeyByID(id)
+    user, err := repository.GetByID(id)
+    if err != nil {
+        return fmt.Errorf("error retrieving user by ID for deletion: %w", err)
+    }
+    repository.client.Delete(idKey)
+    userKey := cacheKeyByUsername(user.Username)
+    repository.client.Delete(userKey)
+    return nil
+}
