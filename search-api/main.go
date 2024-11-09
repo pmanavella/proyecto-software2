f2cce93ea5f6bcc3004bde4c79dad06ba47364be
vsponton <2146222@ucc.edu.ar>
2024-11-09 09:20:39 -0300

    changes

--- search-api/main.go
+++ search-api/main.go
@@ -1,52 +1,52 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
-	"log"
-	"search-api/clients/queues"
-	controllers "search-api/controllers/search"
-	repositories "search-api/repositories/hotels"
-	services "search-api/services/search"
+    "github.com/gin-gonic/gin"
+    "log"
+    "search-api/clients/queues"
+    controllers "search-api/controllers/search"
+    repositories "search-api/repositories/courses"
+    services "search-api/services/search"
 )
 
 func main() {
-	// Solr
-	solrRepo := repositories.NewSolr(repositories.SolrConfig{
-		Host:       "solr",   // Solr host
-		Port:       "8983",   // Solr port
-		Collection: "hotels", // Collection name
-	})
+    // Solr
+    solrRepo := repositories.NewSolr(repositories.SolrConfig{
+        Host:       "solr",    // Solr host
+        Port:       "8983",    // Solr port
+        Collection: "courses", // Collection name
+    })
 
-	// Rabbit
-	eventsQueue := queues.NewRabbit(queues.RabbitConfig{
-		Host:      "rabbitmq",
-		Port:      "5672",
-		Username:  "root",
-		Password:  "root",
-		QueueName: "hotels-news",
-	})
+    // Rabbit
+    eventsQueue := queues.NewRabbit(queues.RabbitConfig{
+        Host:      "rabbitmq",
+        Port:      "5672",
+        Username:  "root",
+        Password:  "root",
+        QueueName: "courses-news",
+    })
 
-	// Hotels API
-	course := repositories.NewHTTP(repositories.HTTPConfig{
-		Host: "hotels-api",
-		Port: "8081",
-	})
+    // Courses API
+    coursesAPI := repositories.NewHTTP(repositories.HTTPConfig{
+        Host: "courses-api",
+        Port: "8081",
+    })
 
-	// Services
-	service := services.NewService(solrRepo, hotelsAPI)
+    // Services
+    service := services.NewService(solrRepo, coursesAPI)
 
-	// Controllers
-	controller := controllers.NewController(service)
+    // Controllers
+    controller := controllers.NewController(service)
 
-	// Launch rabbit consumer
-	if err := eventsQueue.StartConsumer(service.HandleHotelNew); err != nil {
-		log.Fatalf("Error running consumer: %v", err)
-	}
+    // Launch rabbit consumer
+    if err := eventsQueue.StartConsumer(service.HandleCourseNew); err != nil {
+        log.Fatalf("Error running consumer: %v", err)
+    }
 
-	// Create router
-	router := gin.Default()
-	router.GET("/search", controller.Search)
-	if err := router.Run(":8082"); err != nil {
-		log.Fatalf("Error running application: %v", err)
-	}
+    // Create router
+    router := gin.Default()
+    router.GET("/search", controller.Search)
+    if err := router.Run(":8082"); err != nil {
+        log.Fatalf("Error running application: %v", err)
+    }
 }
---
+++ search-api/repositories/courses_http.go
@@ -0,0 +1,53 @@
+package courses
+
+import (
+    "context"
+    "encoding/json"
+    "fmt"
+    "io"
+    "net/http"
+    coursesDomain "search-api/domain/courses"
+)
+
+type HTTPConfig struct {
+    Host string
+    Port string
+}
+
+type HTTP struct {
+    baseURL func(courseID string) string
+}
+
+func NewHTTP(config HTTPConfig) HTTP {
+    return HTTP{
+        baseURL: func(courseID string) string {
+            return fmt.Sprintf("http://%s:%s/courses/%s", config.Host, config.Port, courseID)
+        },
+    }
+}
+
+func (repository HTTP) GetCourseByID(ctx context.Context, id string) (coursesDomain.Course, error) {
+    resp, err := http.Get(repository.baseURL(id))
+    if err != nil {
+        return coursesDomain.Course{}, fmt.Errorf("Error fetching course (%s): %w\n", id, err)
+    }
+    defer resp.Body.Close()
+
+    if resp.StatusCode != http.StatusOK {
+        return coursesDomain.Course{}, fmt.Errorf("Failed to fetch course (%s): received status code %d\n", id, resp.StatusCode)
+    }
+
+    // Read the response body
+    body, err := io.ReadAll(resp.Body)
+    if err != nil {
+        return coursesDomain.Course{}, fmt.Errorf("Error reading response body for course (%s): %w\n", id, err)
+    }
+
+    // Unmarshal the course details into the course struct
+    var course coursesDomain.Course
+    if err := json.Unmarshal(body, &course); err != nil {
+        return coursesDomain.Course{}, fmt.Errorf("Error unmarshaling course data (%s): %w\n", id, err)
+    }
+
+    return course, nil
+}
